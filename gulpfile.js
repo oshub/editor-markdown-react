705c9fbc36de6dfd8d8fb61e0995c7ba91ea3c72
System Administrator <root@MacBook-Pro-de-Carlos.local>
2014-11-28 22:14:07 -0200

    commit inicial

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+gulp.task('style', function(){
+	gulp.src('src/style/**/*.scss')
+	.pipe($.rubySass())
+	.pipe(gulp.dest('build/style'));
+});
+
+gulp.task('react', function() {
+	browserify(['./src/view/app.jsx'])
+    .transform(reactify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe($.uglify())
+    .pipe(gulp.dest('build/jsx'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('src/style/**/*.scss', ['style']);
+	gulp.watch('src/view/**/*.jsx', ['react']);
+});
+
+gulp.task('default', ['style', 'react', 'watch']);
---
+++ src/view/app.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+
+
+
+React.render(
+  ,
+  document.getElementById('main')
+);
